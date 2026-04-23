4f2945202f94b82ecac34fc7f4ab08c5e7870765
Sam Tulkens <sam.t2010@gmail.com>
2026-04-23 15:51:22 +0200

    update inhoud van DeviceUptimeCHeck-Detect.ps1

--- DeviceUptimeCheck-Detect.ps1
+++ DeviceUptimeCheck-Detect.ps1
@@ -6,22 +6,22 @@
     Als de uptime langer is, retourneert het script exit code 1 (remediation nodig).
 .NOTES
     Auteur: Sam T.
-    Datum: 22-04-2026
+    Datum: 23-04-2026
 #>
 
 # Aantal dagen uptime waarna een reboot nodig is
-$maxUptimeDays = 7
+$maxUptimeDays = 1
 
 # Haal de uptime van het device op
 $os = Get-WmiObject Win32_OperatingSystem
-$lastBootUpTime = $os.LastBootUpTime
+$lastBootUpTime = $os.ConvertToDateTime($os.LastBootUpTime)
 $uptime = (Get-Date) - $lastBootUpTime
 
 # Controleer of de uptime langer is dan $maxUptimeDays
 if ($uptime.TotalDays -gt $maxUptimeDays) {
-    Write-Host "Uptime is langer dan $maxUptimeDays dagen. Reboot nodig."
+    Write-Host "Uptime is langer dan $maxUptimeDays dagen. Herstart is nodig."
     exit 1
 } else {
-    Write-Host "Uptime is korter dan $maxUptimeDays dagen. Geen reboot nodig."
+    Write-Host "Uptime is korter dan $maxUptimeDays dagen. Geen herstart nodig."
     exit 0
 }
